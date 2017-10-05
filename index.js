6336c876a5bf5d13b283f5d3a006bbd53e611488
Keller Johnson <kjlt4412@gmail.com>
2017-10-05 17:50:13 -0600

    sql massive node practice

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const massive = require('massive')
+//Get the connection string from Heroku
 const connectionString = "postgres://htzlvpijantviz:0704439e8af1fd342fbfab70e3612a9bff26a9e7c3c11deab67f7268947cab43@ec2-54-235-120-39.compute-1.amazonaws.com:5432/deen421jqsb5di?ssl=true";
 const products_controller = require('./products_controller');
 
